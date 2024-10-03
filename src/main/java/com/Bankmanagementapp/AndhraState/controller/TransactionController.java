9744839ee9779737628d85aa9ed2d80984b29ee1
Yella Krishna Boyene <157083943+boyene@users.noreply.github.com>
2024-10-03 11:41:04 +0530

    Add files via upload

---
+++ src/main/java/com/Bankmanagementapp/AndhraState/controller/TransactionController.java
@@ -0,0 +1,38 @@
+package com.Bankmanagementapp.AndhraState.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.Bankmanagementapp.AndhraState.Entity.Transaction;
+import com.Bankmanagementapp.AndhraState.Service.TransactionService;
+import com.Bankmanagementapp.AndhraState.Types.TransactionType;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/transactions")
+public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+//http://localhost:9976/api/transactions/create
+    @PostMapping("/create")
+    public Transaction createTransaction(@RequestParam Long accountId, @RequestParam TransactionType transactionType, @RequestParam double amount) {
+        return transactionService.createTransaction(accountId, transactionType, amount);
+    }
+
+    @GetMapping("/{transactionId}")
+    public Transaction getTransaction(@PathVariable Long transactionId) {
+        return transactionService.getTransaction(transactionId);
+    }
+
+    @DeleteMapping("/{transactionId}")
+    public void deleteTransaction(@PathVariable Long transactionId) {
+        transactionService.deleteTransaction(transactionId);
+    }
+    //http://localhost:9976/api/transactions/users
+    @GetMapping("users")
+    public List<Transaction> getAllTransactions() {
+        return transactionService.getAllTransactions();
+    }
+}
